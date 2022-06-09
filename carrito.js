5a6a90518726d68a04ac72acd7e218ff0760f181
ferJen <ferjen.coder@gmail.com>
2022-06-08 23:16:26 -0300

    220608-carrito-responsive + js

--- carrito.js
+++ carrito.js
@@ -1,14 +1,34 @@
+/* CARRITO- CONTADOR PRODUCTOS*/
+/* Suma o resta al agregar o quitar productos */
+
+// let numeroFormat = document.querySelectorAll('.numeroFormat');
+
+// for (let i = 0; i <= numeroFormat.length; i++) {
+//   document.querySelectorAll('.numeroFormat')[i].innerHTML = '$ ' + document.querySelectorAll('.numeroFormat')[i].innerHTML.toLocaleString('es-AR');
+// }
+
+// precio.innerHTML = '$' + precio.innerHTML.toLocaleString('es-AR');
+// subtotal.innerHTML = '$' + subtotal.innerHTML.toLocaleString('es-AR');
+
+//CARRITO SECTION
 let productCounter = document.querySelector('.num');
-let subtotal = document.querySelector('.subtotal');
-let precio = document.querySelector('.precio');
+let subtotal = document.querySelector('#subtotal');
+let precio = document.querySelector('#precio');
+
+//CHECKOUT SECTION
+let subtotalCheckOut = document.querySelector('.subtotalCheckOut');
+let impuestosCheckOut = document.querySelector('.impuestosCheckOut');
+let envioCheckOut = document.querySelector('.envioCheckOut');
+let totalCheckOut = document.querySelector('.totalCheckOut');
+
 const buttonMinus = document.querySelector('.min').addEventListener('click', function () {
-  productCounter.innerHTML--;
-  let subtotalP = precio.innerHTML * productCounter.innerHTML;
-  subtotal.innerHTML = subtotalP;
+  if (productCounter.innerHTML > 1) {
+    productCounter.innerHTML--;
+  }
+  subtotal.textContent = '$' + (precio.innerHTML * productCounter.innerHTML).toLocaleString('es-AR');
 });
 
 const buttonPlus = document.querySelector('.plus').addEventListener('click', function () {
-  productCounter.innerHTML++;
-  let subtotalP = precio.innerHTML * productCounter.innerHTML;
-  subtotal.innerHTML = subtotalP;
+  productCounter.innerHTML++; //agregar stop de acuerdo al stock disponible
+  subtotal.textContent = '$' + (precio.innerHTML * productCounter.innerHTML).toLocaleString('es-AR');
 });
