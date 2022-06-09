bb7d1bb387fd675cc23206d2e874fba947b86cb6
ferJen <ferjen.coder@gmail.com>
2022-06-09 01:50:05 -0300

    220608-carrito-responsive + js

--- carrito.js
+++ carrito.js
@@ -1,15 +1,3 @@
-/* CARRITO- CONTADOR PRODUCTOS*/
-/* Suma o resta al agregar o quitar productos */
-
-// let numeroFormat = document.querySelectorAll('.numeroFormat');
-
-// for (let i = 0; i <= numeroFormat.length; i++) {
-//   document.querySelectorAll('.numeroFormat')[i].innerHTML = '$ ' + document.querySelectorAll('.numeroFormat')[i].innerHTML.toLocaleString('es-AR');
-// }
-
-// precio.innerHTML = '$' + precio.innerHTML.toLocaleString('es-AR');
-// subtotal.innerHTML = '$' + subtotal.innerHTML.toLocaleString('es-AR');
-
 //CARRITO SECTION
 let productCounter = document.querySelector('.num');
 let subtotal = document.querySelector('#subtotal');
@@ -21,14 +9,31 @@ let impuestosCheckOut = document.querySelector('.impuestosCheckOut');
 let envioCheckOut = document.querySelector('.envioCheckOut');
 let totalCheckOut = document.querySelector('.totalCheckOut');
 
+//ON LOAD
+// subtotalCheckOut.innerHTML = subtotal.innerHTML;
+// totalCheckOut.innerHTML = parent(subtotalCheckOut.innerHTML) + 0 + 0;
+
+//BUTTON EVENTS
 const buttonMinus = document.querySelector('.min').addEventListener('click', function () {
   if (productCounter.innerHTML > 1) {
     productCounter.innerHTML--;
   }
-  subtotal.textContent = '$' + (precio.innerHTML * productCounter.innerHTML).toLocaleString('es-AR');
+
+  subtotal.innerHTML = precio.innerHTML * productCounter.innerHTML;
+  subtotalCheckOut.innerHTML = parseInt(subtotal.innerHTML) + 0 + 0;
+  totalCheckOut.innerHTML = parseInt(subtotalCheckOut.innerHTML) + 0 + 0;
 });
 
 const buttonPlus = document.querySelector('.plus').addEventListener('click', function () {
   productCounter.innerHTML++; //agregar stop de acuerdo al stock disponible
-  subtotal.textContent = '$' + (precio.innerHTML * productCounter.innerHTML).toLocaleString('es-AR');
+
+  subtotal.innerHTML = precio.innerHTML * productCounter.innerHTML;
+  subtotalCheckOut.innerHTML = parseInt(subtotal.innerHTML) + 0 + 0;
+  totalCheckOut.innerHTML = parseInt(subtotalCheckOut.innerHTML) + 0 + 0;
 });
+
+// let numeroFormat = document.querySelectorAll('.numeroFormat');
+
+// for (let i = 0; i <= numeroFormat.length; i++) {
+//   document.querySelectorAll('.numeroFormat')[i].innerHTML = '$ ' + document.querySelectorAll('.numeroFormat')[i].innerHTML.toLocaleString('es-AR');
+// }
