6f86027437e5a7137ded89915eb2bf5e28cffd5d
ferJen <ferjen.coder@gmail.com>
2022-07-21 20:18:23 -0300

    css/prodjs/html

--- app.js
+++ app.js
@@ -12,46 +12,58 @@ let productoSelected = Number(productoStored.id);
 let favouriteStored = JSON.parse(localStorage.getItem('itemFavourite')) || '';
 let favouriteSelected = Number(favouriteStored.id);
 
-console.log('APP.JS', { cart });
-console.log('APP.JS', { favourites });
+// console.log('APP.JS', { cart });
+// console.log('APP.JS', { favourites });
+// console.log('APP.JS', { productoSelected });
 
 const productList = document.getElementById('product-list');
+
+// CART NAVBAR DROPDOWN
 const cartItemsPill = document.getElementById('cartItemsPill');
-const favouriteItemsPill = document.getElementById('favouriteItemsPill');
 const offcanvasCartDropdown = document.getElementById('offcanvasCartDropdown');
 const navbarCartDropdown = document.getElementById('navbarCartDropdown');
 const cartMenuDropdown = document.getElementById('cartMenuDropdown');
+
+// CART NAVBAR DROPDOWN
 const btnFavourite = document.querySelector('.btnFavourite');
+const favouriteItemsPill = document.getElementById('favouriteItemsPill');
+const offcanvasFavouritesDropdown = document.querySelector('.offcanvasFavouritesDropdown');
+const favouritesMenuDropdown = document.querySelector('.favouritesMenuDropdown');
 
 // AFTER DOM IS LOADED FETCH DATA FROM JSON
 document.addEventListener('DOMContentLoaded', () => {
-  fectchJSONData();
+  fectchDataJSON();
 });
 
-const fectchJSONData = async () => {
+const fectchDataJSON = async () => {
   try {
     const res = await fetch('../assets/js/api.json');
     const dataJSON = await res.json();
     renderOffcanvasCart(dataJSON);
-    console.log(dataJSON);
+    renderFavourites(dataJSON);
+    // console.log('APP.JS FETCH: ', { dataJSON });
   } catch (error) {
-    console.error('Disculpas! algo salió mal. Por favor, intente de nuevo o contacte al admin.');
+    console.error(`Disculpas! algo salió mal. Por favor, intente de nuevo o contacte al admin.`);
     console.error(error);
   }
 };
+
+productList.addEventListener('click', (e) => {
+  selectFavourite(e);
+});
 
 // RENDER OFFCANVAS CART && MENU DROPDOWN CART
 const renderOffcanvasCart = (dataJSON) => {
   offcanvasCartDropdown.innerHTML = '';
   navbarCartDropdown.innerHTML = '';
   cartItemsPill.innerHTML = ''; //cart.length
-  favouriteItemsPill.innerHTML = '';
 
-  if (!favourites.length == null) {
-    for (let i = 0; i < favourites.length; i++) {
+  if (!cart.length == 0) {
+    for (let i = 0; i < cart.length; i++) {
       for (const producto of dataJSON) {
-        if (producto.id == favourites[i].id) {
-          const divHTLM = `
+        if (producto.id == cart[i].id) {
+          console.log(producto.id == cart[i].id);
+          let divHTLM = `
             <li class="list-group-item d-flex justify-content-between align-items-center position-relative">
               <figure class="p-0 m-0 align-self-center">
                 <img class="ps-1" src="${producto.img100[0]}" width="100" height="100" alt="${producto.descripcion}" />
@@ -63,7 +75,9 @@ const renderOffcanvasCart = (dataJSON) => {
                 <p class="m-0 p-0">Precio: ${producto.precio}</p>
               </div>
               <span class="badge bg-success rounded-pill text-dark position-absolute top-0 end-0 m-2">${producto.stock[0]}</span>
-            </li>`;
+            </li>
+            `;
+
           //RENDER PRODUCTS IN MOBILE OFFCANVAS NAVBAR'S CART ICON
           offcanvasCartDropdown.innerHTML += divHTLM;
 
@@ -73,71 +87,85 @@ const renderOffcanvasCart = (dataJSON) => {
       }
     }
   } else {
+    let divHTML = `
+      <li class="list-group-item d-flex justify-content-between align-items-center position-relative">
+        <div class="container d-flex flex-column align-items-start ps-2">
+          <h6 class="text-uppercase pt-1 text-primary m-0">Tu carrito está vacío</h6>
+        </div>
+      </li>
+    `;
+
     //RENDER PRODUCTS IN MOBILE OFFCANVAS NAVBAR'S CART ICON
-    offcanvasCartDropdown.innerHTML = `
-            <li class="list-group-item d-flex justify-content-between align-items-center position-relative">
-              <div class="container d-flex flex-column align-items-start ps-2">
-                <h6 class="text-uppercase pt-1 text-primary m-0">Tu carrito está vacío</h6>
-              </div>
-            </li>
-          `;
+    offcanvasCartDropdown.innerHTML = divHTML;
 
     //RENDER PRODUCTS IN NAVBAR'S CART DROPDOWN
-    navbarCartDropdown.innerHTML = `
-            <li class="list-group-item d-flex justify-content-between align-items-center position-relative">
-              <div class="container d-flex flex-column align-items-start ps-2">
-                <h6 class="text-uppercase pt-1 text-primary m-0">Tu carrito está vacío</h6>
-              </div>
-            </li>
-          `;
+    navbarCartDropdown.innerHTML = divHTML;
   }
 
   cartItemsPill.innerHTML = cart.length; //cart.length
-  favouriteItemsPill.innerHTML = cart.length; //cart.length
   console.log('CART ITEM PILLS', cartItemsPill.innerHTML);
+};
+
+const renderFavourites = (dataJSON) => {
+  offcanvasFavouritesDropdown.innerHTML = '';
+  favouritesMenuDropdown.innerHTML = '';
   console.log('FAVOURITE ITEM PILLS', favouriteItemsPill.innerHTML);
+  favouriteItemsPill.innerHTML = favourites.length; //cart.length
 
-  // FAVOURITES
-  productList.addEventListener('click', (e) => {
+  for (const favourite of dataJSON) {
+    if (favourite.id == favouriteSelected) {
+      // FINISHING MAGIC HAPPENS HERE
+    }
+  }
+
+  // EVENT TO CAPTURE CLICK ON ADD TO FAVOURITES BTN
+  const selectFavourite = function (e) {
+    e.target.stopPropagation;
     e.target.className.includes('btnFavourite') ? setFavourites(e.target.parentElement.parentElement) : '';
-    e.stopPropagation;
-    addToFavourites();
-  });
-
-  const addToFavourites = (producto) => {
-    console.log(producto);
-
-    if (favourites.find((producto) => (producto.id = favouriteSelected))) {
-    } else {
-      favourites.push(producto);
-      localStorage.setItem('favourites', JSON.stringify(favourites));
-      console.log(favourites);
-    }
-  };
-
-  const setFavourites = (e) => {
-    console.log(e);
-    const productoFavourite = {
-      id: e.querySelector('.btnFavourite').dataset.id,
-    };
-    localStorage.setItem('itemFavourite', JSON.stringify(productoFavourite));
-  };
-
-  const callFavourite = (dataJSON) => {
-    for (const producto of dataJSON) {
-      if (producto.id == itemFavourite) {
-        console.log(dataJSON);
-        console.log('Funka');
-      } else {
-        // favourites.push(productoFavourite);
-        localStorage.setItem('favourites', JSON.stringify(favourites));
-      }
-    }
   };
 };
 
+const setFavourites = (e) => {
+  console.log(e);
+  const productoFavourite = {
+    id: e.querySelector('.btnFavourite').dataset.id,
+  };
+  localStorage.setItem('itemFavourite', JSON.stringify(productoFavourite));
+};
+
+// console.log('APP DATAJSON', dataJSON); // UNDEFINED
+
+const addToFavourites = (dataJSON) => {
+  console.log(favouriteSelected);
+  console.log(dataJSON);
+
+  for (const favourite of dataJSON) {
+    if (favourites.find((favourite) => (favourite.id = favouriteSelected))) {
+    } else {
+      favourites.push(favourite);
+      localStorage.setItem('favourites', JSON.stringify(favourites));
+      console.log(favourites);
+    }
+  }
+};
+
+const callFavourite = (dataJSON) => {
+  for (const producto of dataJSON) {
+    if (producto.id == itemFavourite) {
+      console.log(dataJSON);
+      console.log('Funka');
+    } else {
+      // favourites.push(productoFavourite);
+      localStorage.setItem('favourites', JSON.stringify(favourites));
+    }
+  }
+};
+
+//todo ////////////////////////////////////////////////////////////
+//todo // 1. ADD => favouriteItemsPill.innerHTML = '';
+
 //info ////////////////////////////////////////////////////////////
-//info START RESIDUAL CODE TO BE DECIDED
+//info // 1. START RESIDUAL CODE TO BE DECIDED
 
 // Swal.fire({
 //   position: 'bottom-end',
@@ -171,8 +199,8 @@ const renderOffcanvasCart = (dataJSON) => {
 // });
 
 //info END RESIDUAL CODE TO BE DECIDED
-//info ////////////////////////////////////////////////////////////
 
+//info ////////////////////////////////////////////////////////////
 //<<<-- COPYRIGHT ------------------------------------------------------------------------>
 const yearEl = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
